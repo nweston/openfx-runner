da5ecb564bae737b2e2b2d5f0d8d824ef3c90977
Nathan Weston <nweston@fastmail.com>
2023-03-24 17:07:21 -0400

    Print names of plugin bundles.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fs;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    for entry in fs::read_dir("/usr/OFX/Plugins/")? {
+        let path = entry?.path();
+        if path.is_dir() {
+            if let Some(f) = path.file_name() {
+                if f.to_str().map_or(false, |s| s.ends_with(".ofx.bundle")) {
+                    println!("{}", path.display());
+                }
+            }
+        }
+    }
+    return Ok(());
 }
