9f91be724f3defbfdb41747405e5fc908644520e
Nathan Weston <nweston@fastmail.com>
2023-03-24 18:37:23 -0400

    Read plists.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
+use std::error::Error;
 use std::fs;
 
-fn ofx_bundles() -> Vec<std::path::PathBuf> {
+fn plist_path(bundle_path: &std::path::Path) -> std::path::PathBuf {
+    return bundle_path.join("Contents/Info.plist");
+}
+
+struct OfxBundle {
+    path: std::path::PathBuf,
+    plist: plist::Value,
+}
+
+fn make_bundle(path: std::path::PathBuf) -> Result<OfxBundle, Box<dyn Error>> {
+    let plist = plist::Value::from_file(plist_path(&path))?;
+    return Ok(OfxBundle { path, plist });
+}
+
+fn ofx_bundles() -> Vec<OfxBundle> {
     if let Ok(dir) = fs::read_dir("/usr/OFX/Plugins/") {
         let x = dir.filter_map(|entry| {
             let path: std::path::PathBuf = entry.ok()?.path();
             if path.is_dir() {
                 if let Some(f) = path.file_name() {
                     if f.to_str().map_or(false, |s| s.ends_with(".ofx.bundle")) {
-                        return Some(path);
+                        return Some(make_bundle(path).ok()?);
                     }
                 }
             }
@@ -15,11 +30,15 @@ fn ofx_bundles() -> Vec<std::path::PathBuf> {
         });
         return x.collect();
     }
-    return [].to_vec();
+    return Vec::new();
 }
 
 fn main() {
-    for path in ofx_bundles() {
-        println!("{}", path.display())
+    for bundle in ofx_bundles() {
+        println!(
+            "{}, {:?}",
+            bundle.path.display(),
+            bundle.plist.as_dictionary().unwrap()
+        )
     }
 }
