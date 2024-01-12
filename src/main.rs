ac9eefbad3093739e42743aa450b0e1e487c68e3
Nathan Weston <nweston@fastmail.com>
2024-01-11 19:08:29 -0500

    Refactor plugin loading and creation.
    Clean up some duplicated code and make functions more reusable.

--- src/main.rs
+++ src/main.rs
@@ -1121,13 +1121,20 @@ impl<'a> CommandContext<'a> {
     }
 }
 
-fn list_plugins(bundle_name: &str) -> Result<(), Box<dyn Error>> {
+fn load_bundle(
+    bundle_name: &str,
+) -> Result<(Bundle, libloading::Library), Box<dyn Error>> {
     let path = format!("/usr/OFX/Plugins/{}.ofx.bundle", bundle_name);
     let bundle = Bundle::new(path.into()).map_err(|e| GenericError {
         message: format!("Error loading bundle {}", bundle_name),
         source: e,
     })?;
     let lib = bundle.load()?;
+    Ok((bundle, lib))
+}
+
+fn list_plugins(bundle_name: &str) -> Result<(), Box<dyn Error>> {
+    let (_, lib) = load_bundle(bundle_name)?;
     for (i, p) in get_plugins(&lib)?.into_iter().enumerate() {
         println!(
             "{}: {}, v{}.{}",
@@ -1142,12 +1149,7 @@ fn create_plugin(
     plugin_name: &str,
     context: &mut CommandContext,
 ) -> Result<(), Box<dyn Error>> {
-    let path = format!("/usr/OFX/Plugins/{}.ofx.bundle", bundle_name);
-    let bundle = Bundle::new(path.into()).map_err(|e| GenericError {
-        message: format!("Error loading bundle {}", bundle_name),
-        source: e,
-    })?;
-    let lib = bundle.load()?;
+    let (bundle, lib) = load_bundle(bundle_name)?;
     let plugin = get_plugins(&lib)?
         .into_iter()
         .find(|p| p.plugin_identifier == plugin_name)
@@ -1378,39 +1380,18 @@ fn describe(
     bundle_name: &str,
     plugin_name: &str,
     context: &mut CommandContext,
-) -> Result<(), Box<dyn Error>> {
-    let path = format!("/usr/OFX/Plugins/{}.ofx.bundle", bundle_name);
-    let bundle = Bundle::new(path.into()).map_err(|e| GenericError {
-        message: format!("Error loading bundle {}", bundle_name),
-        source: e,
-    })?;
-    let lib = bundle.load()?;
-    let plugin = get_plugins(&lib)?
-        .into_iter()
-        .find(|p| p.plugin_identifier == plugin_name)
-        .ok_or(format!("Plugin {} not found in bundle", plugin_name))?;
-    (plugin.set_host)(context.host);
-    plugin.try_call_action(
-        OfxActionLoad,
-        OfxImageEffectHandle::from(std::ptr::null_mut()),
-        OfxPropertySetHandle::from(std::ptr::null_mut()),
-        OfxPropertySetHandle::from(std::ptr::null_mut()),
-    )?;
+) -> Result<ImageEffect, Box<dyn Error>> {
+    create_plugin(bundle_name, plugin_name, context)?;
 
-    let descriptor = ImageEffect::new(plugin_name);
-    plugin.try_call_action(
+    let plugin = context.get_plugin(plugin_name)?;
+    plugin.plugin.try_call_action(
         OfxActionDescribe,
-        descriptor.clone().into(),
+        plugin.descriptor.clone().into(),
         OfxPropertySetHandle::from(std::ptr::null_mut()),
         OfxPropertySetHandle::from(std::ptr::null_mut()),
     )?;
 
-    println!(
-        "{}",
-        serde_json::to_string(&*descriptor.lock().properties.lock())?
-    );
-
-    Ok(())
+    Ok(plugin.descriptor.lock().clone())
 }
 
 fn process_command(
@@ -1470,7 +1451,11 @@ fn process_command(
         Describe {
             bundle_name,
             plugin_name,
-        } => describe(bundle_name, plugin_name, context),
+        } => {
+            let effect = describe(bundle_name, plugin_name, context)?;
+            println!("{}", serde_json::to_string(&*effect.properties.lock())?);
+            Ok(())
+        }
     }
 }
 
