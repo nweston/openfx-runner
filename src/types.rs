85fdc3ca6dc9d42c99bcb7136c4014ade7b59e07
Nathan Weston <nweston@fastmail.com>
2025-09-05 16:06:43 -0400

    Use handles from openfx_rs

--- src/types.rs
+++ src/types.rs
@@ -3,29 +3,40 @@
 pub use openfx_rs::types::{OfxRangeD, OfxRectD, OfxRectI, OfxStatus, OfxTime};
 use std::ffi::{c_char, c_int, c_uint, c_void};
 
+// Define our own handle types which wrap the openfx_rs versions.
+//
+// This allows us to implement pointer conversions, Hash, and Sync.
 macro_rules! handle {
     ($name: ident) => {
         #[repr(C)]
-        #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
-        pub struct $name(*mut c_void);
+        #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+        pub struct $name(openfx_rs::types::$name);
         impl From<$name> for *mut c_void {
             fn from(handle: $name) -> Self {
-                handle.0
+                handle.0 .0 as _
             }
         }
         impl From<*mut c_void> for $name {
             fn from(ptr: *mut c_void) -> Self {
-                Self(ptr)
+                Self(openfx_rs::types::$name(ptr as _))
             }
         }
         unsafe impl Send for $name {}
+
+        impl std::hash::Hash for $name {
+            fn hash<H>(&self, state: &mut H)
+            where
+                H: std::hash::Hasher,
+            {
+                self.0 .0.hash(state);
+            }
+        }
     };
 }
 
 handle!(OfxImageClipHandle);
 handle!(OfxImageEffectHandle);
 handle!(OfxImageMemoryHandle);
-handle!(OfxMutexConstHandle);
 handle!(OfxMutexHandle);
 handle!(OfxParamHandle);
 handle!(OfxParamSetHandle);
